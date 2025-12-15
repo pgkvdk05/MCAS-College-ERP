b1a82dbbb5f13639a1df8e86fe9a7649b5429021
pgkvdk05 <pgk300505@gmail.com>
2025-12-15 11:21:42 +0530

    [dyad] Added missing React import in main.tsx - wrote 1 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
+import React from "react"; // Added this import
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./globals.css";
 import { SessionContextProvider } from "./components/auth/SessionContextProvider.tsx";
-import { BrowserRouter } from "react-router-dom"; // Import BrowserRouter here
+import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
